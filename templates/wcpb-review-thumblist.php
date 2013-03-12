e9e47620a675c3f5417bd805894656d38b6215b5
Philipp Bauer <philipp.bauer@vividdesign.de>
2013-03-12 22:06:41 +0100

    Added Assets / Updated CSS (SASS)
    Added image assets are used in base64 within the CSS file, binary files
    are only added for archive purposes.

--- templates/wcpb-review-thumblist.php
+++ templates/wcpb-review-thumblist.php
@@ -16,7 +16,7 @@ if ( ! empty( $wcpb->arr_session_data['current_product'] ) ) :
 foreach ( $wcpb->arr_session_data['current_product'] as $arr_optioncat ) :
 foreach ( $arr_optioncat as $int_option_id ) :
 ?>
-	<li style="background-image: url('<?php echo $wcpb->arr_session_data['options'][$int_option_id]['thumbnail_guid']; ?>');">
+	<li class="has-product" style="background-image: url('<?php echo $wcpb->arr_session_data['options'][$int_option_id]['thumbnail_guid']; ?>');">
 		<a href="<?php echo get_permalink( get_the_ID() ) . $str_optioncat_get . "&action=remove_option&optionid=" . $int_option_id; ?>"><?php _e( 'remove ' . $wcpb->arr_session_data['options'][$int_option_id]['the_title'], 'wcpb' ); ?></a>
 	</li>
 <?php
